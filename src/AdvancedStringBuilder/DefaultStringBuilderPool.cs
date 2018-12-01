468a014338859632b4149452494a0348f13d806f
Andrey Taritsyn <taritsyn@gmail.com>
2018-12-01 22:37:18 +0300

    Added a pool of string builders

---
+++ src/AdvancedStringBuilder/DefaultStringBuilderPool.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+using AdvancedStringBuilder.Resources;
+
+namespace AdvancedStringBuilder
+{
+	/// <summary>
+	/// Provides a default implementation of the string builder pool.
+	/// </summary>
+	internal sealed class DefaultStringBuilderPool : StringBuilderPool
+	{
+		/// <summary>
+		/// The default initial capacity of builder.
+		/// </summary>
+		private const int DefaultInitialBuilderCapacity = 100;
+
+		/// <summary>
+		/// The default maximum capacity of builder.
+		/// </summary>
+		private const int DefaultMaxBuilderCapacity = 8 * 1024;
+
+		/// <summary>
+		/// Number of builders per processor.
+		/// </summary>
+		private const int BuilderCountPerProcessor = 5;
+
+		/// <summary>
+		/// Initial capacity of builder.
+		/// </summary>
+		private readonly int _initialBuilderCapacity;
+
+		/// <summary>
+		/// Maximum capacity of builder.
+		/// </summary>
+		private readonly int _maxBuilderCapacity;
+
+		/// <summary>
+		/// First builder.
+		/// </summary>
+		/// <remarks>
+		/// The first builder is stored in a dedicated field, because we expect to be able to satisfy
+		/// most requests from it.
+		/// </remarks>
+		private StringBuilder _firstBuilder;
+
+		/// <summary>
+		/// Array of the retained builders.
+		/// </summary>
+		private readonly StringBuilder[] _builders;
+
+
+		/// <summary>
+		/// Constructs an instance of the default pool of string builders using the default configuration settings.
+		/// </summary>
+		internal DefaultStringBuilderPool()
+			: this(DefaultInitialBuilderCapacity, DefaultMaxBuilderCapacity, Environment.ProcessorCount * BuilderCountPerProcessor)
+		{ }
+
+		/// <summary>
+		/// Constructs an instance of the default pool of string builders using the specified pool size.
+		/// </summary>
+		/// <param name="poolSize">Maximum number of builders stored in the pool.</param>
+		internal DefaultStringBuilderPool(int poolSize)
+			: this(DefaultInitialBuilderCapacity, DefaultMaxBuilderCapacity, poolSize)
+		{ }
+
+		/// <summary>
+		/// Constructs an instance of the default pool of string builders using the specified capacity settings.
+		/// </summary>
+		/// <param name="initialBuilderCapacity">Initial capacity of builder.</param>
+		/// <param name="maxBuilderCapacity">Maximum capacity of builder.</param>
+		internal DefaultStringBuilderPool(int initialBuilderCapacity, int maxBuilderCapacity)
+			: this(initialBuilderCapacity, maxBuilderCapacity, Environment.ProcessorCount * BuilderCountPerProcessor)
+		{ }
+
+		/// <summary>
+		/// Constructs an instance of the default pool of string builders using the specified capacity settings and
+		/// pool size.
+		/// </summary>
+		/// <param name="initialBuilderCapacity">Initial capacity of builder.</param>
+		/// <param name="maxBuilderCapacity">Maximum capacity of builder.</param>
+		/// <param name="poolSize">Maximum number of builders stored in the pool.</param>
+		internal DefaultStringBuilderPool(int initialBuilderCapacity, int maxBuilderCapacity, int poolSize)
+		{
+			if (initialBuilderCapacity <= 0)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(initialBuilderCapacity),
+					Strings.ArgumentOutOfRange_InitialBuilderCapacityNonPositive
+				);
+			}
+
+			if (maxBuilderCapacity <= 0)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(maxBuilderCapacity),
+					Strings.ArgumentOutOfRange_MaxBuilderCapacityNonPositive
+				);
+			}
+
+			if (poolSize <= 0)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(poolSize),
+					Strings.ArgumentOutOfRange_PoolSizeNonPositive
+				);
+			}
+
+			if (initialBuilderCapacity > maxBuilderCapacity)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(initialBuilderCapacity),
+					string.Format(Strings.ArgumentOutOfRange_InitialBuilderCapacityGreaterThanMaxBuilderCapacity,
+						maxBuilderCapacity)
+				);
+			}
+
+			_initialBuilderCapacity = initialBuilderCapacity;
+			_maxBuilderCapacity = maxBuilderCapacity;
+			_builders = new StringBuilder[poolSize - 1];
+		}
+
+
+		public override StringBuilder Rent()
+		{
+			// Examine the first builder.
+			// If that fails, then `RentViaScan` method will look at the remaining builders.
+			StringBuilder builder = _firstBuilder;
+			if (builder == null || builder != Interlocked.CompareExchange(ref _firstBuilder, null, builder))
+			{
+				builder = RentViaScan();
+			}
+
+			return builder;
+		}
+
+		public override StringBuilder Rent(int capacity)
+		{
+			if (capacity <= _maxBuilderCapacity)
+			{
+				return Rent();
+			}
+
+			return new StringBuilder(capacity);
+		}
+
+		[MethodImpl((MethodImplOptions)256 /* AggressiveInlining */)]
+		private StringBuilder RentViaScan()
+		{
+			StringBuilder[] builders = _builders;
+			int builderCount = builders.Length;
+
+			for (int builderIndex = 0; builderIndex < builderCount; builderIndex++)
+			{
+				StringBuilder builder = builders[builderIndex];
+				if (builder != null)
+				{
+					if (builder == Interlocked.CompareExchange(ref builders[builderIndex], null, builder))
+					{
+						return builder;
+					}
+				}
+			}
+
+			return new StringBuilder(_initialBuilderCapacity);
+		}
+
+		public override void Return(StringBuilder builder)
+		{
+			if (builder == null || builder.Capacity > _maxBuilderCapacity)
+			{
+				return;
+			}
+
+			if (_firstBuilder == null)
+			{
+				builder.Clear();
+				_firstBuilder = builder;
+			}
+			else
+			{
+				ReturnViaScan(builder);
+			}
+		}
+
+		[MethodImpl((MethodImplOptions)256 /* AggressiveInlining */)]
+		private void ReturnViaScan(StringBuilder builder)
+		{
+			StringBuilder[] builders = _builders;
+			int builderCount = builders.Length;
+
+			for (int builderIndex = 0; builderIndex < builderCount; builderIndex++)
+			{
+				if (builders[builderIndex] == null)
+				{
+					builder.Clear();
+					builders[builderIndex] = builder;
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ src/AdvancedStringBuilder/StringBuilderPool.cs
@@ -0,0 +1,111 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+#if NET40
+
+using PolyfillsForOldDotNet.System.Threading;
+#endif
+
+namespace AdvancedStringBuilder
+{
+	/// <summary>
+	/// Provides a resource pool that enables reusing instances of type <see cref="StringBuilder"/>.
+	/// </summary>
+	public abstract class StringBuilderPool
+	{
+		/// <summary>
+		/// The lazily-initialized shared pool instance.
+		/// </summary>
+		private static StringBuilderPool s_sharedInstance = null;
+
+		/// <summary>
+		/// Retrieves a shared <see cref="StringBuilderPool"/> instance.
+		/// </summary>
+		public static StringBuilderPool Shared
+		{
+			[MethodImpl((MethodImplOptions)256 /* AggressiveInlining */)]
+			get
+			{
+				return Volatile.Read(ref s_sharedInstance) ?? EnsureSharedCreated();
+			}
+		}
+
+
+		/// <summary>
+		/// Ensures that <see cref="s_sharedInstance"/> has been initialized to a pool and returns it.
+		/// </summary>
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		private static StringBuilderPool EnsureSharedCreated()
+		{
+			Interlocked.CompareExchange(ref s_sharedInstance, Create(), null);
+			return s_sharedInstance;
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="StringBuilderPool"/> instance using the default configuration settings.
+		/// </summary>
+		/// <returns>A new <see cref="StringBuilderPool"/> instance.</returns>
+		public static StringBuilderPool Create()
+		{
+			return new DefaultStringBuilderPool();
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="StringBuilderPool"/> instance using the specified pool size.
+		/// </summary>
+		/// <param name="poolSize">Maximum number of builders stored in the pool.</param>
+		/// <returns>A new <see cref="StringBuilderPool"/> instance with the specified pool size.</returns>
+		public static StringBuilderPool Create(int poolSize)
+		{
+			return new DefaultStringBuilderPool(poolSize);
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="StringBuilderPool"/> instance using the specified capacity settings.
+		/// </summary>
+		/// <param name="initialBuilderCapacity">Initial capacity of builder.</param>
+		/// <param name="maxBuilderCapacity">Maximum capacity of builder.</param>
+		/// <returns>A new <see cref="StringBuilderPool"/> instance with the specified capacity settings.</returns>
+		public static StringBuilderPool Create(int initialBuilderCapacity, int maxBuilderCapacity)
+		{
+			return new DefaultStringBuilderPool(initialBuilderCapacity, maxBuilderCapacity);
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="StringBuilderPool"/> instance using the specified capacity settings and
+		/// pool size.
+		/// </summary>
+		/// <param name="initialBuilderCapacity">Initial capacity of builder.</param>
+		/// <param name="maxBuilderCapacity">Maximum capacity of builder.</param>
+		/// <param name="poolSize">Maximum number of builders stored in the pool.</param>
+		/// <returns>A new <see cref="StringBuilderPool"/> instance with the specified capacity settings and
+		/// pool size.</returns>
+		public static StringBuilderPool Create(int initialBuilderCapacity, int maxBuilderCapacity, int poolSize)
+		{
+			return new DefaultStringBuilderPool(initialBuilderCapacity, maxBuilderCapacity, poolSize);
+		}
+
+		/// <summary>
+		/// Retrieves a instance of string builder from the pool.
+		/// </summary>
+		/// <returns>Instance of string builder.</returns>
+		public abstract StringBuilder Rent();
+
+		/// <summary>
+		/// Retrieves a instance of string builder with at least the given capacity from the pool.
+		/// </summary>
+		/// <remarks>
+		/// If the capacity is less than or equal to our maximum capacity, then return builder from the pool.
+		/// Otherwise create a new string builder, that will just get discarded when released.
+		/// </remarks>
+		/// <param name="capacity">Capacity of string builder.</param>
+		/// <returns>Instance of string builder.</returns>
+		public abstract StringBuilder Rent(int capacity);
+
+		/// <summary>
+		/// Returns a instance of string builder to the pool.
+		/// </summary>
+		/// <param name="builder">Instance of string builder.</param>
+		public abstract void Return(StringBuilder builder);
+	}
+}
